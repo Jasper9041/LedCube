0578f884f87c336888c670f6b6cdb9cdefc833d5
Jasper Beaurain <info@jasperbeaurain.com>
2020-10-05 15:35:35 +0200

    initial commit

---
+++ LedCube2.0/FrameBuffer.cpp
@@ -0,0 +1,63 @@
+#include "FrameBuffer.h"
+
+
+namespace geometricFunc{
+    const float GetYForLine(uint8_t x, uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2){
+        return (((y2-y1)/(x2-x1))*(x-x1))+y1;
+    }
+}
+
+FrameBuffer::FrameBuffer(){
+    std::bitset<NUMBER_OF_LEDS_PER_LAYER> tmp_bitset;
+    for (size_t i = 0; i < NUMBER_OF_LAYERS ; i++){
+        leds.push_back(tmp_bitset);
+    }
+}
+
+std::vector<uint8_t> FrameBuffer::TranslateLayerFromBitset(uint8_t layer) const{
+    std::vector<uint8_t> out_vec;
+    // out_vec.push_back(1 << (layer + 4));
+    out_vec.push_back(1 << layer);
+    uint8_t tmp_byte = 0;
+    for(size_t i = 0; i < NUMBER_OF_LEDS_PER_LAYER; i++){
+        if(i == NUMBER_OF_LEDS_PER_LAYER - 1){
+            tmp_byte = tmp_byte | leds[layer][i];
+            out_vec.push_back(tmp_byte);
+        }
+        else if (i != 0 && i % 8 == 0){
+            out_vec.push_back(tmp_byte);
+            tmp_byte = leds[layer][i] << 7;
+        } else {
+            tmp_byte = tmp_byte | leds[layer][i] << (7-(i % 8)); 
+        } 
+    }
+    return out_vec;
+}
+
+void FrameBuffer::SetBit(bool new_state, uint8_t x, uint8_t y, uint8_t z){
+    uint8_t bit_location = y*NUMBER_OF_LEDS_PER_AXIS + x;
+    leds[z][bit_location] = new_state;
+}
+
+void FrameBuffer::SetBitsInFullLayer(bool new_state, uint8_t layer){
+    for(size_t i = 0; i < NUMBER_OF_LEDS_PER_LAYER; i++){
+        leds[layer][i] = new_state;
+    }
+}
+
+void FrameBuffer::Draw2DLine(bool new_state, uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, uint8_t z){
+    if (x2-x1 > y2-y1){
+        for(uint8_t x = x1; x <= x2; x++){
+            float y = geometricFunc::GetYForLine(x, x1, y1, x2, y2);
+            uint8_t rounded_y = round(y);
+            SetBit(new_state, x, y, z);
+        }
+    }
+    else{
+        for(uint8_t y = y1; y <= y2; y++){
+            float x = geometricFunc::GetYForLine(y, y1, x1, y2, x2);
+            uint8_t rounded_y = round(x);
+            SetBit(new_state, x, y, z);
+        }
+    }
+}
